a2e224ec92a0f05a70071b5d7ae66d6a824d5d56
name <mahimathacker75@gmail.com>
2023-10-18 12:08:16 +0530

    BrainGym

---
+++ xhibiter/data/dropdown.js
@@ -0,0 +1,35 @@
+export const collectionDropdown2_data = [
+	{
+		id: '01:1 sessions',
+		text: '1:1 sessions',
+		image: '/images/avatars/collection_ava_1.png',
+	},
+	{
+		id: '1Priority DMs',
+		text: 'Priority DMs',
+		image: '/images/avatars/collection_ava_1.png',
+	},
+	{
+		id: '2Digital Product',
+		text: 'Digital Product',
+		image: '/images/avatars/collection_ava_1.png',
+	},
+	
+];
+export const EthereumDropdown2_data = [
+	{
+		id: '0Ethereum',
+		text: 'Ethereum',
+		image: '/images/chains/ETH.png',
+	},
+	{
+		id: '1Flow',
+		text: 'Flow',
+		image: '/images/chains/FLOW.png',
+	},
+	{
+		id: '2FUSD',
+		text: 'FUSD',
+		image: '/images/chains/FUSD.png',
+	},
+];
