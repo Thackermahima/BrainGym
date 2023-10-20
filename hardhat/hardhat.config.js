151b4681d03060c570c9a7f96ba13349737c83a5
Dhruv Sathavara <dhruvsathavara7228@gmail.com>
2023-10-20 17:45:51 +0530

    deployed_on_scroll

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -6,14 +6,27 @@ const PRIVATE_KEY = process.env.NEXT_PUBLIC_PRIVATE_KEY;
 console.log('q node url', QUICKNODE_HTTP_URL);
 console.log('p key', PRIVATE_KEY);
 
+// module.exports = {
+//   solidity: "0.8.20",
+//   networks: {
+//     mumbai: {
+      
+//       // url: QUICKNODE_HTTP_URL,
+//       url: "https://lively-spring-market.matic-testnet.quiknode.pro/d5245b604fd92b7050a13e1f9383f11b4c546438/",
+//       accounts: [PRIVATE_KEY],
+//     },
+//   },
+// };
+
+
+
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    mumbai: {
-      
-      // url: QUICKNODE_HTTP_URL,
-      url: "https://lively-spring-market.matic-testnet.quiknode.pro/d5245b604fd92b7050a13e1f9383f11b4c546438/",
-      accounts: [PRIVATE_KEY],
+    scrollSepolia: {
+      url: "https://sepolia-rpc.scroll.io/" || "",
+      accounts:
+      [PRIVATE_KEY],
     },
   },
 };
